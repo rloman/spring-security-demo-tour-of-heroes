470f17d1ad94cf73ab701fc847aec48d463f7a7e
Raymond Loman <raymond@carpago.nl>
2017-11-01 17:37:32 +0100

    Move bean for CORS filter from JerseyConfig (which is not a very bad location though) to beans package to have it clearer in view

--- src/main/java/nl/carpago/tourofheroes/config/JerseyConfig.java
+++ src/main/java/nl/carpago/tourofheroes/config/JerseyConfig.java
@@ -3,11 +3,7 @@ package nl.carpago.tourofheroes.config;
 import javax.ws.rs.ApplicationPath;
 
 import org.glassfish.jersey.server.ResourceConfig;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
 import nl.carpago.tourofheroes.rest.HeroesEndpoint;
 
@@ -18,26 +14,5 @@ public class JerseyConfig extends ResourceConfig {
    public JerseyConfig() {
       
       register(HeroesEndpoint.class);
-      
    }
-   
-   @Bean
-   public CorsFilter corsFilter() {
-      
-       UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-       CorsConfiguration config = new CorsConfiguration();
-       
-       config.setAllowCredentials(true);
-       config.addAllowedOrigin("*");
-       config.addAllowedHeader("*");
-       config.addAllowedMethod("OPTIONS");
-       config.addAllowedMethod("GET");
-       config.addAllowedMethod("POST");
-       config.addAllowedMethod("PUT");
-       config.addAllowedMethod("DELETE");
-       source.registerCorsConfiguration("/**", config);
-       
-       return new CorsFilter(source);
-   }
-   
 }
