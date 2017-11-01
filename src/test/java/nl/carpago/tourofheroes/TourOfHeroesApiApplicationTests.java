b6c7a60bff040e729ede5eac253e3e9ebcdc3c7b
Raymond Loman <raymond@carpago.nl>
2017-11-01 17:05:35 +0100

    Initial load

---
+++ src/test/java/nl/carpago/tourofheroes/TourOfHeroesApiApplicationTests.java
@@ -0,0 +1,16 @@
+package nl.carpago.tourofheroes;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TourOfHeroesApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
