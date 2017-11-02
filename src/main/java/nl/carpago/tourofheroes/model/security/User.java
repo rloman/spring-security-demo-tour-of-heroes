351b6830ae0416bef3ddc2240aa8f885f6e0ca69
Raymond Loman <raymond@carpago.nl>
2017-11-02 08:26:39 +0100

    Some simpler|

--- src/main/java/nl/carpago/tourofheroes/model/security/User.java
+++ src/main/java/nl/carpago/tourofheroes/model/security/User.java
@@ -43,4 +43,35 @@ public class User {
     public void setAuthorities(List<Authority> authorities) {
         this.authorities = authorities;
     }
+
+   @Override
+   public int hashCode() {
+      final int prime = 31;
+      int result = 1;
+      result = prime * result + ((id == null) ? 0 : id.hashCode());
+      return result;
+   }
+
+   @Override
+   public boolean equals(Object obj) {
+      if (this == obj) {
+         return true;
+      }
+      if (obj == null) {
+         return false;
+      }
+      if (!(obj instanceof User)) {
+         return false;
+      }
+      User other = (User) obj;
+      if (id == null) {
+         if (other.id != null) {
+            return false;
+         }
+      }
+      else if (!id.equals(other.id)) {
+         return false;
+      }
+      return true;
+   }
 }
