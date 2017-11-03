bcec9f28e826ffaf55a03f1317dcad0f1af5b580
Raymond Loman <raymond@carpago.nl>
2017-11-03 14:37:09 +0100

    Enable console for later (does not rock since I am using security here)

--- src/main/java/nl/carpago/tourofheroes/config/WebSecurityConfig.java
+++ src/main/java/nl/carpago/tourofheroes/config/WebSecurityConfig.java
@@ -59,6 +59,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
                 .authorizeRequests()
                 .antMatchers("/auth/**").permitAll()
+                .antMatchers("/h2-console/**").permitAll()
                 .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                 .anyRequest().authenticated();
 
